5a28dbab4103d99acc5c60f55ab5514678d1972d
Michael Rinderle <michael@sofdigital.net>
2021-10-23 15:36:52 -0500

    Add project files.

---
+++ ctemplate/ctemplate.cpp
@@ -0,0 +1,12 @@
+﻿// ctemplate.cpp : Defines the entry point for the application.
+//
+
+#include "ctemplate.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
